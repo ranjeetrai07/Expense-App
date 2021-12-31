0e1b3c73ee1b74300cfd117b3b94993c4e273f6a
ranjeetrai07 <49452565+ranjeetrai07@users.noreply.github.com>
2021-12-31 08:54:57 +0530

    Add files via upload

---
+++ expense-project/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,107 @@
+import React, {useState} from "react";
+//import ExpenseDate from "../Expenses/ExpenseDate";
+
+import './ExpenseForm.css'
+
+const ExpenseForm = (props) => {
+    //this is for multiple states
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) =>{
+        setEnteredTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) =>{
+        setEnteredAmount(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+    
+
+    
+   //this is for one state
+//    const [userInput, setUserInput] = useState({
+//         enteredTitle: '',
+//         enteredAmount: '',
+//         enteredDate: ''
+//    });
+   
+   
+   //this is for one state(this will not coorect way to do when we depend on previous state)
+//    const titleChangeHandler = (event) =>{
+//        setUserInput({
+//            ...userInput,
+//            enteredTitle: event.target.value,
+//        })
+//    };
+
+   //const titleChangeHandler = (event) =>{
+    //  setUserInput({
+    //    ...userInput,
+    //    setEnteredAmount: event.target.value,
+    //    });
+
+    //alter native way when we depen on previous state
+    //setUserInput((prevState) => {
+    //       return{ ...prevState, enteredTitle: event.target.value};
+    // });
+    //};
+
+//    const amountChangeHandler = (event) =>{
+//     setUserInput({
+//         ...userInput,
+//         setEnteredAmount: event.target.value,
+//         })
+//     };
+
+    // const dateChangeHandler = (event) =>{
+    //     setUserInput({
+    //         ...userInput,
+    //         setEnteredDate: event.target.value,
+    //     })
+    // };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        props.onSaveExpenseData(expenseData);
+        //console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
+            </div> 
+                <div className='new-expense__actions'>
+                    <button type="submit">Add Expense</button>
+                </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
